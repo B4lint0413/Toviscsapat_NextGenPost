280be7883b8b27e6b1fdf96ce008a04fdea3b455
B4lint0413 <69457665+B4lint0413@users.noreply.github.com>
2022-01-27 21:18:47 +0100

    Files are commented and formatted well

--- Toviscsapat_NextGenPost/MyBlocks.py
+++ Toviscsapat_NextGenPost/MyBlocks.py
@@ -27,24 +27,23 @@ def Drive(speed, cm, steering):
 def MedMotor(speed, angle, port, transmission):
     #For negative rotation negate the speed
     #For port give the name of the wanted motor
+
     ev3 = EV3Brick()
     szum = 0 
     if port == "D":
         motor = Motor(Port.D, positive_direction=Direction.CLOCKWISE)
     elif port == "A":
         motor = Motor(Port.A, positive_direction=Direction.CLOCKWISE)
+    
+    #Counting transmission
     for i in transmission:
         if transmission.index(i) != 0:
             szum = szum / i
         else:
             szum = i
-    # if len(transmission)%2 == 0{
-    #     speed = Speed*-1
-    # }else{
-    #     speed = Speed
-    # }
     TargRot = angle/szum
-    # motor.reset_angle(0)
+
+    #Moving the motor
     if speed > 0:
         while abs(motor.angle()) < TargRot:
             motor.run(speed)
@@ -55,6 +54,7 @@ def MedMotor(speed, angle, port, transmission):
 
 def NoGyroTU (speed, angle):
     # 12 cm a kerekek közti táv
+    #Turns the robot without the usage of gyro sensor
     distance = 12
     ev3 = EV3Brick()
     diameter = 14.34
@@ -62,8 +62,6 @@ def NoGyroTU (speed, angle):
     BMotor = Motor(Port.B, positive_direction=Direction.COUNTERCLOCKWISE)
     CMotor = Motor(Port.C, positive_direction=Direction.COUNTERCLOCKWISE)
 
-    # BMotor.reset_angle(0)
-    # CMotor.reset_angle(0)
     while (abs(BMotor.angle())/360 <= TargRotation):
         BMotor.run(speed)
         CMotor.run(-speed)
@@ -71,11 +69,14 @@ def NoGyroTU (speed, angle):
     CMotor.stop()    
 
 def AbsTU(toAngle, speed, gyro):
-    #
+    #Absolute turning which means that the given angle is not relative, it is always the same direction
+    #North is 0, east is 90, south is 180 and west is 270
     ev3 = EV3Brick()
     BMotor = Motor(Port.B, positive_direction=Direction.COUNTERCLOCKWISE)
     CMotor = Motor(Port.C, positive_direction=Direction.COUNTERCLOCKWISE)
     startAng = (360+(gyro.angle()%360))%360
+    
+    #Chosing the shorter way to turn (turning right or left)
     if(toAngle-startAng > 180):
         while (((360+(gyro.angle()%360))%360) != toAngle):
             BMotor.run((startAng-toAngle)/abs(toAngle-startAng)*speed)
@@ -86,7 +87,10 @@ def AbsTU(toAngle, speed, gyro):
             CMotor.run((startAng-toAngle)/abs(toAngle-startAng)*speed)
     BMotor.stop()
     CMotor.stop()
+
 def AbsDRV(toAngle, speed, cm, gyro):
+    #It is the same as AbsTU, but it moves the robot forward (or backwards).
+    #It was made to correct the AbsTU if gyro sensor wasn't precise. It inspects whether the robot is still in the wanted direction.
     #To negate the AbsDRV negate the speed and the cm too
     ev3 = EV3Brick()
     BMotor = Motor(Port.B, positive_direction=Direction.COUNTERCLOCKWISE)
@@ -95,11 +99,7 @@ def AbsDRV(toAngle, speed, cm, gyro):
     DRV = DriveBase(BMotor, CMotor, 81.6, 105)
     CM = cm
     TargetRot = ((BMotor.angle()+CMotor.angle())/2)+CM/(8.16*3.1416)*360
-    #startAng = (360+(gyro.angle()%360))%360
-    #gyro.reset_angle(startAng)
 
-    # BMotor.reset_angle(0)
-    # CMotor.reset_angle(0)
     if CM > 0:
         while ((BMotor.angle() + CMotor.angle())/2 < TargetRot):
             DRV.drive(Speed, toAngle-(gyro.angle()%360))
@@ -107,18 +107,17 @@ def AbsDRV(toAngle, speed, cm, gyro):
         while ((BMotor.angle() + CMotor.angle())/2 > TargetRot):
             DRV.drive(Speed, toAngle-(gyro.angle()%360))
     DRV.stop()
+
 def Align(sensor1, sensor2):
     #Feature idea: calibrate the definition of black(reflected) before running
+    #It aligns the robot to a black line to make robot run more precise.
     # sensor1 is the sensor on the left side; sensor2 is the sensor on the right side
     ev3 = EV3Brick()
     BMotor = Motor(Port.B, positive_direction=Direction.COUNTERCLOCKWISE)
     CMotor = Motor(Port.C, positive_direction=Direction.COUNTERCLOCKWISE)
     DRV = DriveBase(BMotor, CMotor, 81.6, 105)
-    #startAng = (360+(gyro.angle()%360))%360
-    #gyro.reset_angle(startAng)
 
-    # BMotor.reset_angle(0)
-    # CMotor.reset_angle(0)
+    #It is slowing down as it approaches the black line.
     while sensor1.reflection()>=10 or sensor2.reflection()>=10:
         if sensor1.reflection()>=10:
             BMotor.run(sensor1.reflection()*1)#speed
@@ -132,6 +131,10 @@ def Align(sensor1, sensor2):
     CMotor.stop()
 
 def LFOL(sensor1, sensor2, speed, cm, k, mode):
+    #PD (from PID controller) line following, to make smooth and precise line following.
+    #We don't use the "I" factor, because it would be useful for longer line followings, for example at least 2 meters.
+    #"P" is responsible for the error, the differrence between what we see with the sensor, and what we want to see.
+    #We use "D" to take  last error in account to "predict" what will happen.
     # K is responsible for the amount of correction
     # sensor1 is the sensor on the left side; sensor2 is the sensor on the right side
     # Modes: mode 1: both sensors; mode 2: left sensor from right
@@ -144,11 +147,7 @@ def LFOL(sensor1, sensor2, speed, cm, k, mode):
     TargetRot = ((BMotor.angle()+CMotor.angle())/2)+CM/(8.16*3.1416)*360
     steering = 0
     lastError = 0
-    #startAng = (360+(gyro.angle()%360))%360
-    #gyro.reset_angle(startAng)
 
-    # BMotor.reset_angle(0)
-    # CMotor.reset_angle(0)
     if mode == 1:
         while ((BMotor.angle() + CMotor.angle())/2 < TargetRot):
             error = (40-sensor2.reflection())+(sensor1.reflection()-40)
--- Toviscsapat_NextGenPost/main.py
+++ Toviscsapat_NextGenPost/main.py
@@ -17,7 +17,7 @@ from pybricks.media.ev3dev import SoundFile, ImageFile, Image, Font
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
 
-# Write your program here.
+# Declare the sensors and motors, and reset them (we reset them just here, and it has a reason).
 ev3 = EV3Brick()
 gyro = GyroSensor(Port.S4, Direction.COUNTERCLOCKWISE)
 gyro.reset_angle(0)
@@ -28,218 +28,72 @@ BMotor = Motor(Port.B, positive_direction=Direction.COUNTERCLOCKWISE)
 CMotor = Motor(Port.C, positive_direction=Direction.COUNTERCLOCKWISE)
 DMotor = Motor(Port.D, positive_direction=Direction.CLOCKWISE)
 
-
-# AbsTU(30, 100, gyro)
-# wait(1000)
-# AbsDRV(30, 100, 65, gyro)
-# wait(1000)
-# AbsTU(90, 100, gyro)
-# wait(1000)
-# AbsDRV(90, 100, 120, gyro)
-# wait(1000)
-# AbsTU(180, 100, gyro)
-# wait(1000)
-# AbsDRV(180, 100, 25, gyro)
-# wait(1000)
-# AbsTU(270, 100, gyro)
-
-#Containers round
-MedMotor(-200, 120, "D", [8, 18])
-AbsDRV(30, 100, 25, gyro)
-LFOL(color1, color2, 70, 100, 1, 4)
-# AbsDRV(100, 100, 70, gyro)
-# wait(1000000)
-LFOL(color1, color2, 70, 40, 0.7, 3)
-wait(1500)
-print(gyro.angle())
-#This goes to the rails:
-# AbsTU(225, 100, gyro)
-# print(gyro.angle())
-# wait(1500)
-# AbsDRV(225, -100, -20, gyro)
-# # print(gyro.angle())
-# AbsTU(180, 100, gyro)
-# LFOL(color1, color2, 70, 30, 2, 2)
-# wait(1000)
-# MedMotor(100, 35, "A", [1, 2])
-# wait(1500)
-# MedMotor(-100, 35, "A", [1, 2])
-# AbsDRV(180, -100, -15, gyro)
-# wait(1000)
-# AbsTU(270, 100, gyro)
-# wait(1000)
-# AbsDRV(270, 100, 15, gyro)
-# wait(1000)
-AbsTU(183, 100, gyro)
-MedMotor(100, 50, "A", [1, 2])
-AbsDRV(183, 200, 48, gyro)
-wait(2000)
-MedMotor(-100, 42, "A", [1, 2])
-AbsDRV(180, -100, -35, gyro)
-AbsTU(90, 100, gyro)
-AbsDRV(75, -100, -255, gyro)
-
-#Airplane round
-# print(gyro.angle())
-# AbsDRV(0, 100, 20, gyro)
-# print(gyro.angle())
-# LFOL(color1, color2, 100, 50, 1.3, 3)
-# print(gyro.angle())
-# Align(color1, color2)
-# print(gyro.angle())
-# MedMotor(130, 200, "D", [1, 1.2])
-# wait(1000)
-# MedMotor(-130, 200, "D", [1, 1.2])
-# MedMotor(130, 200, "A", [1, 1.2])
-# print(gyro.angle())
-# AbsDRV(60, 100, 13, gyro)
-# MedMotor(-130, 100, "A", [1, 1.2])
-# MedMotor(130, 80, "A", [1, 1.2])
-# print(gyro.angle())
-# AbsDRV(45, -150, -20, gyro)
-# AbsDRV(45, -200, -50, gyro)
-
-# Western round
-# wait(1000)
-# MedMotor(200, 90, "A", [12, 22])
-# Drive(200, 64, 0)
-# wait(1000)
-# NoGyroTU(-200, 55)
-# wait(1000)
-# MedMotor(200, 150, "D", [12, 20])
-# wait(1000)
-# MedMotor(-200, 80, "D", [12, 20])
-# wait(1000)
-# NoGyroTU(200, 130)
-# wait(1000)
-# MedMotor(200, 80, "D", [12, 20])
-# wait(1000)
-# Drive(200, 15, 0)
-# wait(1000)
-# MedMotor(-200, 60, "D", [12, 20])
-
-# Second round
-# AbsDRV(0, 50, 15, gyro)
-# wait(1000)
-# #LFOL 177cm
-# LFOL(color1, color2, 80, 80, 1.3, 5)
-# wait(500)
-# MedMotor(100, 167, "A", [1.67, 1])
-# wait(500)
-# LFOL(color1, color2, 80, 60, 1.3, 5)
-# wait(500)
-# MedMotor(100, 105, "A", [1.67, 1])
-# wait(500)
-# LFOL(color1, color2, 80, 50, 1.3, 5)
-# wait(500)
-# MedMotor(100, 180, "D", [1, 1])
-# wait(500)
-# LFOL(color1, color2, 80, 20, 1.3, 5)
-
-# NoGyroTU(200, 230)
-# wait(1000)
-# AbsTU(90, 100, gyro)
-# wait(1000)
-# AbsDRV(90, 200, 30, gyro)
-
-# Master Menu
-# Somehow it looks like the is_alive() part of threading doesn't work with EV3 Micropython, so I try a more single minded method, using a boolean variable, isRunning
 isRunning = False
-
 
 def Running(Runs):
     global isRunning
     isRunning = Runs
 
-
+#The actual rounds
 def Round(nth):
-    # isRunning = True
     if nth == 1:
         print("First round")
-        # time.sleep(10)
-        AbsTU(30, 10, gyro)
-        wait(1000)
-        AbsDRV(10, 10, 65, gyro)
-        wait(1000)
-        AbsTU(90, 10, gyro)
-        wait(1000)
-        AbsDRV(10, 10, 120, gyro)
-        wait(1000)
-        AbsTU(180, 10, gyro)
-        wait(1000)
-        AbsDRV(10, 10, 25, gyro)
-        wait(1000)
-        AbsTU(270, 10, gyro)
-        wait(1000)
-        MedMotor(-50, 50, DMotor, gyro, [10])
-        wait(1000)
-        MedMotor(50, 50, DMotor, gyro, [90])
-        wait(1000)
-        AbsTU(180, 10, gyro)
-        wait(1000)
-        MedMotor(-10, 10, AMotor, gyro, [1])
-        wait(1000)
-        AbsDRV(10, 10, -25, gyro)
-        wait(1000)
-        AbsTU(270, 10, gyro)
-        wait(1000)
-        AbsDRV(10, 10, 200, gyro)
-        wait(1000)
-    elif nth == 2:
+        MedMotor(-200, 120, "D", [8, 18])
+        AbsDRV(30, 100, 25, gyro)
+        LFOL(color1, color2, 70, 100, 1, 4)
+        LFOL(color1, color2, 70, 40, 0.7, 3)
+        wait(1500)
+        print(gyro.angle())
+        AbsTU(183, 100, gyro)
+        MedMotor(100, 50, "A", [1, 2])
+        AbsDRV(183, 200, 48, gyro)
+        wait(2000)
+        MedMotor(-100, 42, "A", [1, 2])
+        AbsDRV(180, -100, -35, gyro)
+        AbsTU(90, 100, gyro)
+        AbsDRV(75, -100, -255, gyro)
+   elif nth == 2:
         print("Second round")
-        # time.sleep(10)
         AbsDRV(0, 50, 15, gyro)
         wait(1000)
-        # LFOL 177cm
+        #LFOL 177cm
         LFOL(color1, color2, 80, 80, 1.3, 5)
         wait(500)
-        MedMotor(100, 167, AMotor, [1.67, 1])
+        MedMotor(100, 167, "A", [1.67, 1])
         wait(500)
         LFOL(color1, color2, 80, 60, 1.3, 5)
         wait(500)
-        MedMotor(100, 105, AMotor, [1.67, 1])
+        MedMotor(100, 105, "A", [1.67, 1])
         wait(500)
         LFOL(color1, color2, 80, 50, 1.3, 5)
         wait(500)
-        MedMotor(100, 180, DMotor, [1, 1])
+        MedMotor(100, 180, "D", [1, 1])
         wait(500)
         LFOL(color1, color2, 80, 20, 1.3, 5)
     elif nth == 3:
         print("Third round")
-        # time.sleep(10)
+        AbsDRV(0, 100, 20, gyro)
+        LFOL(color1, color2, 100, 50, 1.3, 3)
+        Align(color1, color2)
+        MedMotor(130, 200, "D", [1, 1.2])
         wait(1000)
-        MedMotor(200, 90, AMotor, [12, 22])
-        Drive(200, 64, 0)
-        wait(1000)
-        NoGyroTU(-200, 55)
-        wait(1000)
-        MedMotor(200, 150, DMotor, [12, 20])
-        wait(1000)
-        MedMotor(-200, 80, DMotor, [12, 20])
-        wait(1000)
-        NoGyroTU(200, 130)
-        wait(1000)
-        MedMotor(200, 80, DMotor, [12, 20])
-        wait(1000)
-        Drive(200, 15, 0)
-        wait(1000)
-        MedMotor(-200, 60, DMotor, [12, 20])
+        MedMotor(-130, 200, "D", [1, 1.2])
+        MedMotor(130, 200, "A", [1, 1.2])
+        AbsDRV(60, 100, 13, gyro)
+        MedMotor(-130, 100, "A", [1, 1.2])
+        MedMotor(130, 80, "A", [1, 1.2])
+        AbsDRV(45, -150, -20, gyro)
+        AbsDRV(45, -200, -50, gyro)
     Running(False)
 
-
-# from rounds import Round
+#Setting up the display
 curRound = 1
 Rounds = ["Containers", "Tower", "Airplane", "Last"]
 screen = Image("Screen", sub=False)
 font = Font(family=None, size=14)
 ev3.screen.set_font(font)
-# p = Process(target=Round, args=(curRound))
-# p.start()
-# p.join()
-# t1 = threading.Thread(target=Round, args=(curRound))
-# t1.start()
-# if __name__=="__main__":
-# savedData = DataLog("1", name="CurrentRound", timestamp=False, extension="txt")
+
+#Setting up the text file
 text = open("CurrentRound.txt", "r")
 txt = text.readlines()
 print(txt)
@@ -247,68 +101,46 @@ print(len(txt))
 curRound = int(txt[len(txt)-1].replace("\n", ""))
 text.close()
 Text = open("CurrentRound.txt", "w")
+
+#The (infinite) main loop
 while True:
     Text.write(str(curRound)+"\n")
-    # textFile = open("CurrentRound.txt", "w")
-    # savedData.log(curRound)
-    # print(savedData)
     sleep(0.3)
-    # textFile.close()
-    # print(int(textFile.read()))
-    # print(t1.is_alive())
-    # print(isRunning)
-    # print(curRound)
+    
+    #Center button
     if Button.CENTER in ev3.buttons.pressed() and not isRunning:
         gyro.reset_angle(0)
-        # color1.reset()
-        # color2.reset()
         AMotor.reset_angle(0)
         BMotor.reset_angle(0)
         CMotor.reset_angle(0)
         DMotor.reset_angle(0)
         ev3.speaker.play_file(SoundFile.READY)
-        # isRunning = True
-        # Round(curRound), stop()
-        # isRunning = False
-        # p = Process(target=Round, args=(curRound))
-        # p.start()
         Round(curRound)
-        # p.join()
-        # if __name__=="__main__":
         sleep(0.5)
         if curRound < 4:
             curRound += 1
-            # print("Stop")
-            # Stop()
-            # isRunning = False
+
+    #Upper button
     elif Button.UP in ev3.buttons.pressed() and curRound < 4 and not isRunning:
         curRound += 1
         sleep(0.3)
+
+    #Downer button
     elif Button.DOWN in ev3.buttons.pressed() and curRound > 1 and not isRunning:
         curRound -= 1
         sleep(0.3)
-    elif Button.RIGHT in ev3.buttons.pressed() and not isRunning: #curRound == 1 and 
-        # if __name__=="__main__":
-        # savedData.log("1")
+
+    #Right button, which exits the program and resets the current round to the first
+    elif Button.RIGHT in ev3.buttons.pressed() and not isRunning:  
         Text.write("1\n")
         Text.close()
         break
-        sleep(0.5)
-        # p.join()
-        # isRunning = True
-        # Round(5), stop()
-        # isRunning = False
-        curRound += 1
-    elif Button.LEFT_UP in ev3.buttons.pressed() and not isRunning: #curRound == 1 and 
-        # if __name__=="__main__":
-        # p.join()
+
+    #Left up (back) button, which exits and saves the last known "current round", and for next it starts with that saved round 
+    elif Button.LEFT_UP in ev3.buttons.pressed() and not isRunning:
         Text.close()
-        print("bal gomb")
         break
-        # isRunning = True
-        # Round(6), stop()
-        # isRunning = False
-        curRound += 1
+    
+    #Refreshing the screen
     ev3.screen.clear()
-    # note: font size is too big; ignore mulitprocessing and save current round in a text file instead; do restart function with file-reading method
     ev3.screen.draw_text(0, 40, "Current round: "+Rounds[curRound-1])
